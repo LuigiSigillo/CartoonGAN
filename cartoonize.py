48b80bab8bac5fbb81fcd55894ae0108dba0c620
LuigiSigillo <luigisigillo30@gmail.com>
2021-03-11 17:53:15 +0100

    fixed

--- cartoonize.py
+++ cartoonize.py
@@ -4,31 +4,38 @@ import os
 import PIL
 import sys
 import glob
-import imageio
 import argparse
 import numpy as np
 from tqdm import tqdm
 from datetime import datetime
+import tensorflow as tf
+from itertools import product
+from imageio import imwrite,imread
+import gc
+from IPython.display import Image,display
 
-STYLES = ["shinkai", "hayao", "paprika"]
+def get_dataset(batch_size=1):
+    from glob import glob
 
+    files = glob(os.path.join(input_dir, "*") )
+    num_images = len(files)
+    ds = tf.data.Dataset.from_tensor_slices(files)
+    ds = ds.shuffle(num_images)
+    ds = ds.repeat()
 
-parser = argparse.ArgumentParser(description="transform real world images to specified cartoon style(s)")
-parser.add_argument("--styles", nargs="+", default=[STYLES[0]],
-                    help="specify (multiple) cartoon styles which will be used to transform input images.")
-parser.add_argument("--all_styles", action="store_true",
-                    help="set true if all styled results are desired")
-parser.add_argument("--batch_size", type=int, default=1,
-                    help="number of images that will be transformed in parallel to speed up processing. "
-                         "higher value like 4 is recommended if there are gpus.")
-parser.add_argument("--comparison_view", type=str, default="smart",
-                    choices=["smart", "horizontal", "vertical", "grid"],
-                    help="specify how input images and transformed images are concatenated for easier comparison")
+    def fn(filename):
+        x = tf.io.read_file(filename)
+        x = tf.image.decode_jpeg(x, channels=3)
+        img = tf.cast(x, tf.float32) / 127.5 - 1
+        #print("\n tipo img = ",type(img),"\n tipo x = ",type(x), filename, type(filename))
+        return img
 
-
-args = parser.parse_args()
-
-
+    ds = ds.map(fn, batch_size)
+    ds = ds.batch(batch_size)
+    
+    steps = int(np.ceil(num_images/batch_size))
+    # user iter(ds) to avoid generating iterator every epoch
+    return iter(ds), steps
 
 def loadmodel(problem, checkpoint=False):
     models_dir="models"
@@ -49,37 +56,9 @@ def loadmodel(problem, checkpoint=False):
     return model
 
 
-def pre_processing(image_path, style, expand_dim=True):
-    input_image = PIL.Image.open(image_path).convert("RGB")
-
-    if not args.keep_original_size:
-        width, height = input_image.size
-        aspect_ratio = width / height
-        resized_height = min(height, args.max_resized_height)
-        resized_width = int(resized_height * aspect_ratio)
-        if width != resized_width:
-            input_image = input_image.resize((resized_width, resized_height))
-
-    input_image = np.asarray(input_image)
-    input_image = input_image.astype(np.float32)
-
-    input_image = input_image[:, :, [2, 1, 0]]
-
-    if expand_dim:
-        input_image = np.expand_dims(input_image, axis=0)
-    return input_image
-
 def result_exist(image_path, style):
-    return os.path.exists(os.path.join(args.output_dir, style, image_path.split("/")[-1]))
+    return os.path.exists(os.path.join(output_dir, style, image_path.split("/")[-1]))
 
-def post_processing(transformed_image, style):
-    if not type(transformed_image) == np.ndarray:
-        transformed_image = transformed_image.numpy()
-    transformed_image = transformed_image[0]
-    transformed_image = transformed_image[:, :, [2, 1, 0]]
-    transformed_image = transformed_image * 0.5 + 0.5
-    transformed_image = transformed_image * 255
-    return transformed_image
 
 def save_transformed_image(output_image, img_filename, save_dir):
     if not os.path.exists(save_dir):
@@ -94,75 +73,62 @@ def save_transformed_image(output_image, img_filename, save_dir):
     return transformed_image_path
 
 
-def save_concatenated_image(image_paths, image_folder="comparison", num_columns=2):
-    images = [PIL.Image.open(i).convert('RGB') for i in image_paths]
-    # pick the image which is the smallest, and resize the others to match it (can be arbitrary image shape here)
-    min_shape = sorted([(np.sum(i.size), i.size) for i in images])[0][1]
-    array = np.asarray([np.asarray(i.resize(min_shape)) for i in images])
+def show_and_save_images(batch_x, image_name, nrow=1, ncol=1,to_be_saved=True):
+    if not isinstance(batch_x, np.ndarray):
+        batch_x = batch_x.numpy()
+    n, h, w, c = batch_x.shape
+    out_arr = np.zeros([h * nrow, w * ncol, 3], dtype=np.uint8)
+    for (i, j), k in zip(product(range(nrow), range(ncol)), range(n)):
+        out_arr[(h * i):(h * (i+1)), (w * j):(w * (j+1))] = batch_x[k]
+    path_name = os.path.join(output_dir,image_name)
+    if to_be_saved:
+        imwrite(path_name, out_arr)
+    gc.collect()
+    return path_name
 
-    view = args.comparison_view
-    if view == "smart":
-        width, height = min_shape[0], min_shape[1]
-        aspect_ratio = width / height
-        grid_suitable = (len(args.styles) + 1) % num_columns == 0
-        is_portrait = aspect_ratio <= 0.75
-        if grid_suitable and not is_portrait:
-            view = "grid"
-        elif is_portrait:
-            view = "horizontal"
-        else:
-            view = "vertical"
-
-    if view == "horizontal":
-        images_comb = np.hstack(array)
-    elif view == "vertical":
-        images_comb = np.vstack(array)
-    elif view == "grid":
-        rows = np.split(array, num_columns)
-        rows = [np.hstack(row) for row in rows]
-        images_comb = np.vstack([row for row in rows])
-
-    images_comb = PIL.Image.fromarray(images_comb)
-    file_name = image_paths[0].split("/")[-1]
-
-    if args.output_dir not in image_folder:
-        image_folder = os.path.join(args.output_dir, image_folder)
-    if not os.path.exists(image_folder):
-        os.makedirs(image_folder)
-
-    image_path = os.path.join(image_folder, file_name)
-    images_comb.save(image_path)
-    return image_path
+STYLES = ["shinkai", "hayao", "paprika"]
+VALID_EXTENSIONS = ['jpg', 'png', 'gif', 'JPG']
+input_dir = "input_images"
+output_dir = "output_images"
+parser = argparse.ArgumentParser(description="transform real world images to specified cartoon style(s)")
+parser.add_argument("--styles", nargs="+", default=[STYLES[0]],
+                    help="specify (multiple) cartoon styles which will be used to transform input images.")
+parser.add_argument("--all_styles", action="store_true",
+                    help="set true if all styled results are desired")
+parser.add_argument("--batch_size", type=int, default=1,
+                    help="number of images that will be transformed in parallel to speed up processing. "
+                         "higher value like 4 is recommended if there are gpus.")
+parser.add_argument("--comparison_view", type=str, default="smart",
+                    choices=["smart", "horizontal", "vertical", "grid"],
+                    help="specify how input images and transformed images are concatenated for easier comparison")
 
 
-if __name__ =="main":
-    styles = STYLES if args.all_styles else args.styles
-    models = list()
-    for style in styles:
-        models.append(loadmodel(style))
-    
-    
-    progress_bar = tqdm(image_paths, desc='Transforming')
-    for image_path in progress_bar:
-        image_filename = image_path.split("/")[-1]
-        progress_bar.set_postfix(File=image_filename)
+args = parser.parse_args()
 
-        related_image_paths = [image_path]
-        for model, style in zip(models, styles):
-            input_image = pre_processing(image_path, style=style)
-            save_dir = os.path.join(args.output_dir, style)
-            return_existing_result = result_exist(image_path, style)
 
-            if not return_existing_result:
-                transformed_image = model(input_image)
-                output_image = post_processing(transformed_image, style=style)
-                transformed_image_path = save_transformed_image(output_image, image_filename, save_dir)
-            else:
-                transformed_image_path = save_transformed_image(None, image_filename, save_dir)
 
-            related_image_paths.append(transformed_image_path)
 
-        if not args.skip_comparison:
-            save_concatenated_image(related_image_paths)
-    progress_bar.close()
+styles = STYLES if args.all_styles else args.styles
+models = list()
+for style in styles:
+    models.append(loadmodel(style))
+image_paths = []
+for ext in VALID_EXTENSIONS:
+    image_paths.extend(glob.glob(os.path.join(input_dir, f"*.{ext}")))
+
+progress_bar = tqdm(image_paths, desc='Transforming')
+for image_path in progress_bar:
+    image_filename = image_path.split("/")[-1]
+    progress_bar.set_postfix(File=image_filename)
+
+    related_image_paths = [image_path]
+    for model, style in zip(models, styles):
+        dataset, steps_per_epoch = get_dataset()
+        input_image = dataset.next()
+        save_dir = os.path.join(output_dir, style)
+        return_existing_result = result_exist(image_path, style)
+        #transformed_image_path = save_transformed_image(transformed_image, image_filename, save_dir)
+        show_and_save_images(tf.cast((model(input_image)+1) * 127.5, tf.uint8), image_name=(image_filename), ncol=1,nrow=1)
+        
+progress_bar.close()
 
